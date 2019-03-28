3c54497331240d9b061f3f6215a38532af2c7fb7
asangurima <asangurima@gmail.com>
2019-03-28 17:14:57 -0400

    Add handlebars for viewing, updating, deleting photo jars
    - Create user-images.handlebars
    - Create viewUserFileSuccess in ui.js to call on user-images.handlebars
    
    @juliojesse25 @asangurima

--- assets/scripts/resource/ui.js
+++ assets/scripts/resource/ui.js
@@ -2,10 +2,16 @@
 'use strict'
 // const store = require('../store.js')
 const showAllImagesTemplate = require('../templates/all-images.handlebars')
+const showUserImagesTemplate = require('../templates/user-images.handlebars')
 
 const uploadFileSuccess = (data) => {
   const showImagesHtml = showAllImagesTemplate({ images: data.images })
   $('#content').html(showImagesHtml)
+}
+
+const viewUserFileSuccess = (data) => {
+  const showUserImagesHtml = showUserImagesTemplate({ images: data.images })
+  $('#content').html(showUserImagesHtml)
 }
 
 const failure = () => {
@@ -17,5 +23,6 @@ const failure = () => {
 
 module.exports = {
   uploadFileSuccess,
+  viewUserFileSuccess,
   failure
 }
